d8577a85e4915acc10ef1449f5c5219f26627518
Brendo Moreira <brendo_prime@hotmail.com>
2023-05-23 15:41:45 -0300

    configurações(styled, axios, lock.jason) aplicadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components"
 import HomePage from "./pages/HomePage/HomePage"
-import SeatsPage from "./pages/SeatsPage/SeatsPage"
-import SessionsPage from "./pages/SessionsPage/SessionsPage"
-import SuccessPage from "./pages/SuccessPage/SuccessPage"
+// import SeatsPage from "./pages/SeatsPage/SeatsPage"
+// import SessionsPage from "./pages/SessionsPage/SessionsPage"
+// import SuccessPage from "./pages/SuccessPage/SuccessPage"
 
 export default function App() {
     return (
         <>
-           <NavContainer>CINEFLEX</NavContainer>
+            <NavContainer>CINEFLEX</NavContainer>
 
             <HomePage />
             {/* <SeatsPage /> */}
